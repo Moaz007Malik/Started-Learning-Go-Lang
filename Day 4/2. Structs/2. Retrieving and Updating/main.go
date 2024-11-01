3287e87474be272ec18cb46ebdc7133a5b8ba940
Moaz Malik <moaz007malik@gmail.com>
2024-11-01 19:56:15 +0500

    Day 4

--- Day 4/2. Structs/2. Retrieving and Updating/main.go
+++ Day 4/2. Structs/2. Retrieving and Updating/main.go
@@ -30,4 +30,42 @@ func main() {
 	myBook := aBook
 	myBook.year = 2020
 	fmt.Println(myBook, aBook)
+
+	// Anonymous structs
+
+	diana := struct {
+		firstName, lastName string
+		age                 int
+	}{
+		firstName: "Diana",
+		lastName:  "Muller",
+		age:       30,
+	}
+	fmt.Printf("%#v\n", diana)
+	fmt.Printf("Diana's age is %d\n", diana.age)
+
+	type book struct {
+		string
+		float64
+		bool
+	}
+
+	b1 := book{"1984 by George Orwell", 10.2, false}
+	fmt.Printf("%#v\n", b1)
+
+	fmt.Println(b1.string)
+
+	type Employee struct {
+		name string
+		salary int
+		bool
+	}
+
+	// e := Employee{name: "John", salary: 1000, bool: true}
+	e := Employee{"John", 1000, false}
+	fmt.Printf("%#v\n", e)
+
+	e.bool = true
+
+	fmt.Printf("%#v\n", e)
 }
